7b82f93e76a97be4a292eb78675b3eee0824b156
Noelle Kosarek <noelle.noreen.kosarek@dartmouth.edu>
2023-07-26 14:37:16 -0400

    Add In Class Assignment
    Add in Class Assignment Data Analytics I and Solutions.

--- data/20230621_DataAnalyticsI_SampleData.R
+++ data/20230621_DataAnalyticsI_SampleData.R
@@ -8,9 +8,10 @@ for (i in 1:5){
 	race <- sample(c("Black","White", "Asian","Hispanic/Latino","Other"), 100, replace = TRUE)
 	SES <- sample(c("Low","Medium","High"), 100, replace = TRUE)
 	disease_status <- sample(c("Diseased","Not Disease"), 100, replace = TRUE)
+	crp <- rnorm(100, mean=0.3, sd=0.1)
 	
-	df <- data.frame(participant, age, sex, race, SES, disease_status)
+	df <- data.frame(participant, age, sex, race, SES, disease_status, crp)
 	
-	write.csv(df, paste0("/Users/noellekosarek/Documents/GitHub/QBS103/data/Data_Analytics_1_Lesson_Data/sample_data_",i,".csv"), row.names = F)
+	write.csv(df, paste0("/Users/noellekosarek/Documents/GitHub/QBS103/data/Data_Analytics_1_In_Class_Assignment_Data/sample_data_",i,".csv"), row.names = F)
 	i = i +1
 }
